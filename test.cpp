bc26c3964509d211b4c7e46c19465064e5acbf52
sean.lee <sean.lee@endoquestrobotics.com>
2024-02-08 22:16:38 +0900

    just test

--- test.cpp
+++ test.cpp
@@ -1,1 +1,22 @@
-afd
+#include <vector>
+#include <iostream>
+
+int main()
+{
+
+	std::vector<int> a{1,2,3};
+	std::vector<int> b{4,5,6};	
+	std::vector<int> tmp;
+
+
+    tmp.insert(tmp.end(), a.begin(), a.end());
+    tmp.insert(tmp.end(), b.begin(), b.end());
+
+	for(auto& item : tmp){
+		std::cout<< item <<" ";
+	}
+	std::cout<<std::endl;
+
+
+	return 0;
+}
